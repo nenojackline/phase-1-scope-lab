bf9a777d70b934ddf67a679b080a50aeb178afe7
jackline chepngeno <chepngenojackline0@gmail.com>
2022-06-09 07:33:21 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+}
+
+var bestCustomer;
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"
+}
+
+
+const least = "Leon"
+
+function changeLeastFavoriteCustomer() {
+    least = "Jacque"
+}
